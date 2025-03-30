2f7eca4f707da892133018fb1b68111a66ed321b
Sidney Schultz <tewzdk@gmail.com>
2025-03-30 20:44:09 +0200

    update angular to 19

--- src/app/pages/books/books.component.ts
+++ src/app/pages/books/books.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-books',
-  standalone: true,
-  imports: [],
-  templateUrl: './books.component.html',
-  styleUrl: './books.component.scss'
+    selector: 'app-books',
+    imports: [],
+    templateUrl: './books.component.html',
+    styleUrl: './books.component.scss'
 })
 export class BooksComponent {
 
